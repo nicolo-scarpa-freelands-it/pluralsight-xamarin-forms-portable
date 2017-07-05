d893d00d986c939ea08a90dccf68db981e180bc8
Nicolò Scarpa <nicolo.scarpa@freelands.it>
2017-07-05 17:11:26 +0200

    Providing UI Behavor with Views and XAML

---
+++ XFPortableClassLibraryApp/MyPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace XFPortableClassLibraryApp
+{
+    public partial class MyPage : ContentPage
+    {
+        public MyPage()
+        {
+            InitializeComponent();
+
+            boxViewColor.Color = Color.Blue;
+        }
+
+        void Handle_ValueChanged(object sender, Xamarin.Forms.ValueChangedEventArgs e)
+        {
+			var red = sliderRed.Value;
+			var green = sliderGreen.Value;
+			var blue = sliderBlue.Value;
+
+			boxViewColor.Color = Color.FromRgb(red, green, blue);
+        }
+    }
+}
